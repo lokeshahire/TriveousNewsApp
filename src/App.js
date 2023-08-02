2dc91b199fc6cd1a12d33344c832cfa88785bccd
Lokesh <iamlokeshahire@gmail.com>
2023-08-03 00:34:46 +0530

    newsApp assignment done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import { useSelector } from "react-redux";
 import Register from "./components/Register";
 import Login from "./components/Login";
@@ -14,23 +10,30 @@ import NewsDetail from "./components/NewsDetail";
 const App = () => {
   const { user } = useSelector((state) => state.auth);
 
-  const PrivateRoute = ({ path, element }) => {
-    return (
-      <Route path={path}>
-        <React.Fragment>{element}</React.Fragment>
-      </Route>
-    );
-  };
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={(props) => (user ? <Component {...props} /> : <Login />)}
+    />
+  );
 
   return (
     <Router>
       <div>
-        <h1 className="app-title">Triveous News App</h1>
+        <h1
+          style={{
+            fontSize: "50px",
+            textAlign: "center",
+            color: "#E81828",
+          }}
+        >
+          Triveous News App
+        </h1>
         <Routes>
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/news" element={NewsList} />
-          <Route path="/news/:title" element={NewsDetail} />
+          <Route path="/news" element={<NewsList />} />
+          <Route path="/news/:title" element={<NewsDetail />} />
         </Routes>
       </div>
     </Router>
